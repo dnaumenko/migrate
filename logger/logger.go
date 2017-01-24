7e27aaadf107543c8dac2654a6f180f02d68beb2
dnaumenko <dm.naumenko@gmail.com>
2017-01-24 15:46:13 +0200

    fix compile error

--- logger/logger.go
+++ logger/logger.go
@@ -21,5 +21,5 @@ type Logger interface {
 
 // DefaultLogger returns a default implementation for a Logger interface
 func DefaultLogger() *Logger {
-	return &log.New(os.Stderr, "", log.LstdFlags)
+	return log.New(os.Stderr, "", log.LstdFlags)
 }
