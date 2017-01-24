dff6493728003add6c69e22ef39a1c6fe65e1003
dnaumenko <dm.naumenko@gmail.com>
2017-01-24 15:39:17 +0200

    add logger interface

---
+++ logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"os"
+	"log"
+)
+
+type Logger interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+	Println(...interface{})
+
+	Fatal(...interface{})
+	Fatalf(string, ...interface{})
+	Fatalln(...interface{})
+
+	Panic(...interface{})
+	Panicf(string, ...interface{})
+	Panicln(...interface{})
+}
+
+// DefaultLogger returns a default implementation for a Logger interface
+func DefaultLogger() *Logger {
+	return &log.New(os.Stderr, "", log.LstdFlags)
+}
